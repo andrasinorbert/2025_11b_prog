b40513daae97fdf4df004cd82c7030d5a0794719
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2026-01-28 13:24:40 +0100

    20260128

---
+++ JS/20260128/index.js
@@ -0,0 +1,34 @@
+let x=5;
+let y="5";
+
+console.log(x+y);
+
+function fuggvenynev(n1,n2){
+    return n1+n2;
+}
+
+console.log(fuggvenynev(3,4));
+
+let tomb=[1,2,"szzia", false];
+
+console.log(tomb);
+
+for(let i=0; i<tomb.length; i++){
+    console.log(tomb[i]);
+}
+
+let z=5;
+
+if(z%2==0){
+    console.log("z páros");
+}
+else{
+    console.log("z páratlan");
+}
+
+// egysoros
+
+/*
+több soros
+komment
+*/
