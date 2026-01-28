689b4638ffed34467b2ad2fd9519511e77c5a5d6
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2026-01-28 14:32:07 +0100

    20260128

--- JS/20260128/index.js
+++ JS/20260128/index.js
@@ -32,3 +32,71 @@ else{
 több soros
 komment
 */
+
+// óra vége: prog tételek megírása önállóan
+
+
+function max_kiv(tomb){
+    let max_index=0;
+    let max_ert=tomb[0];
+    for(let i=1; i<tomb.length; i++){
+        if(max_ert<tomb[i]){
+            max_ert=tomb[i];
+            max_index= i;
+        }
+    }
+    return [max_index, max_ert];
+}
+
+let t1=[2,3,0,3,8,0,6,8,5];
+
+function q(x){
+    console.log(x);
+}
+
+q(max_kiv(t1));
+
+function szelso_ertek_kiv(tomb, func){
+    let index=0;
+    let ert=tomb[0];
+    for(let i=1; i<tomb.length; i++){
+        if(func(ert,tomb[i])){
+            ert=tomb[i];
+            index= i;
+        }
+    }
+    return [index, ert];
+}
+
+q(szelso_ertek_kiv(t1, (n1,n2)=>n1>n2)); //minimum kiv
+q(szelso_ertek_kiv(t1, (n1,n2)=>n1<n2)); //maximum kiv
+
+
+q(szelso_ertek_kiv(t1, (n1,n2)=>n1>=n2)); //utolsó minimum kiv
+q(szelso_ertek_kiv(t1, (n1,n2)=>n1<=n2)); //utolsó maximum kiv
+
+
+let zzz=5; //lokális változó
+
+var xxxx=15; //globális változó
+
+const KONSTANS="szia";
+
+const PI=3.14; //nagybetűs változó név = const
+
+//PI+=6; // hiba!
+
+let objektum= {
+    name: "Zoli",
+    kor: 3,
+    getName() {
+        return this.name;
+    },
+    getKor() {
+        return 32;
+    }
+}
+
+q(objektum.name);
+q(objektum.getName());
+q(objektum.getKor());
